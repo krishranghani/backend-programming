ae85ca7fb01cbea07e602744a8d2f93f8e50c49a
krish ranghani <krishranghani@gmail.com>
2024-03-01 07:59:33 +0530

    30-02-2024

--- express_js/app2.js
+++ express_js/app2.js
@@ -19,7 +19,7 @@ app.post('/products',(req, res)=>{
 });
 
 app.get('/products',(req, res)=>{
-    res.status(200).json(products);
+    res.status(200).json(products); 
 });
 
 app.get('/products/single-product',(req, res)=>{
@@ -27,6 +27,39 @@ app.get('/products/single-product',(req, res)=>{
     // console.log(id);
     let product = products.find((item)=>item.id === id)
     res.status(200).json(product);
+});
+
+// Replace Single Product
+
+app.put('/products/replace-product', (req, res) => {
+const id = +req.query.id;
+let productIndex = products.findIndex((item) => item.id === id)
+let product = products [productIndex];
+products.splice(productIndex, 1, { ...req.body });
+// console.log(product);
+res.status(200).json({ message: 'Product Replace SuccessFully.....' });
+});
+
+// Update Single Product
+
+app.patch('/products/update-product', (req, res) => {
+const id = +req.query.id;
+let productIndex = products.findIndex((item) => item.id === id)
+let product = products [productIndex];
+let item = products.splice(productIndex, 1, { ...product, ...req.body });
+// console.log(product);
+res.status(200).json({ message: 'Product Update SuccessFully.....' });
+});
+
+// Delete Single Product
+
+app.delete('/products/delete-product', (req, res) => {
+const id = +req.query.id;
+let productIndex = products.findIndex((item) => item.id === id)
+let product = products [productIndex];
+let item = products.splice(productIndex, 1);
+// console.log(product);
+res.status(200).json({message: 'Product Delete Successfully.....' });
 });
 
 
